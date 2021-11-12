6f722892ca5041a5d6792a401ad8e3ca5f82de82
Fortune00 <tasha0415@naver.com>
2021-11-12 17:39:06 +0900

    [feat] base code of InSearch

---
+++ InSearch/InSearch.py
@@ -0,0 +1,51 @@
+
+class InSearch:
+    def __init__(self):
+        # key : value = string token : list document_id
+        self.table = {}
+
+    # table에 document의 token과 id를 추가
+    # parameter : int document_id, string document
+    # return : boolean
+    def add_document(self, document_id, document):
+        # 해당 id가 없는지 확인
+        # document 형태소 분석
+        # table에 { token : document id }를 추가
+        #   token이 없으면, 새로이 추가
+        #   token이 있으면, 원래의 id list에 해당 id값만 추가
+        return True
+
+    # table에 document의 token과 id를 삭제
+    # parameter : int document_id, string document
+    # return : boolean
+    def delete_document(self, document_id, document):
+        # 해당 id가 있는지 확인
+        # document 형태소 분석
+        # table의 해당 token을 찾아 token과 document_id를 삭제
+        #   해당 token이
+        return True
+
+    # table의 해당 document의 기록을 new_document로 변경
+    # parameter : int document_id
+    # return : boolean
+    def update_document(self, document_id, new_document):
+        # delete_document(document_id, new_document)
+        # add_document(document_id, new_document)
+        return True
+
+    # 전체 table을 딕셔너리로 반환
+    # parameter : int document_id
+    # return : Dictionary table
+    def return_table(self):
+        return self.table
+
+    # 검색어와 가장 부합한 document의 id를 table에서 찾아 반환
+    # parameter : query
+    # return : list document_id
+    def search(self, query):
+        return True
+
+    # table 초기화
+    # return : boolean
+    def delete_all(self):
+        return True
