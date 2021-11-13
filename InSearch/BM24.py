febe600c693ba146cac3536bf796579005e7e10d
Kwon <ddft97@gmail.com>
2021-11-13 21:33:30 +0900

    [feat] get_avg_document_len 구현
    bm24 메소드에서 받아오는 매개변수가 달라짐에 따라 avg_document_len를 bm24.py에서 직접 계산.
    상수를 나타내는 변수명 대문자로 변경

--- InSearch/BM24.py
+++ InSearch/BM24.py
@@ -1,24 +1,40 @@
 import numpy as np
 from . import TokenAnalyzer as ta
 
+
     # IDF값 계산
     # parameter : int document_count, int fq_document
     # return : float
 def IDF(document_count, fq_document):
     return np.log(1 + ((document_count - fq_document + 0.5)/fq_document + 0.5))
 
+
+    # document들의 평균 길이 계산
+    # parameter : dic document_len, int list document_id, int document_count
+    # return : float
+def get_avg_document_len(document_len_dic, document_id_list, document_count):
+    total_length = 0
+    for id in document_id_list:
+        total_length += document_len_dic[id]
+    return total_length / document_count
+
+
     # full-text(query)에 대해 각 document의 점수 계산
-    # parameter : InSearch table, int list document_id, string query, float avgdl
+    # parameter : InSearch table, string query, int list document_id, dic document_len
     # return : float list
-def bm24(table, document_id, query, avgdl):
+def bm24(table, query, document_id_list, document_len_dic):
     score_list = []
-    k1 = 1.2
-    b = 0.75
-    document_count = len(document_id)
+    # K1은 1.2 ~ 2.0 사이의 상수
+    K1 = 1.2
+    # B는 0.75인 상수
+    B = 0.75
+    document_count = len(document_id_list)
+    # document들의 평균 길이 계산
+    avgdl = get_avg_document_len(document_len_dic, document_id_list, document_count)
     # query문을 형태소 분석
     query_token = ta.token_analyzer(query)
     # 각 document마다 반복
-    for id in document_id:
+    for id in document_id_list:
         score = 0
         # query문의 각 형태소마다 반복
         for token in query_token:
@@ -33,9 +49,8 @@ def bm24(table, document_id, query, avgdl):
             # IDF
             token_idf = IDF(document_count, fq_document)
             # TF
-            token_tf = (fq_count * (k1 + 1))/(fq_count + k1 * (1 - b + b * (len(document_id)/avgdl)))
+            token_tf = (fq_count * (K1 + 1))/(fq_count + K1 * (1 - B + B * (len(document_id_list)/avgdl)))
             score += token_idf * token_tf
         # score(D,Q) = sigma(IDF*TF)
         score_list.append((score, id))
-
     return score_list
