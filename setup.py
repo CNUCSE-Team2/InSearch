6b70de20c510e6031498b17d389477c79042d10d
Fortune00 <tasha0415@naver.com>
2021-11-14 19:55:01 +0900

    [setup] for release

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import setup, find_packages
+from os import path
+
+this = path.abspath(path.dirname(__file__))
+with open(path.join(this, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name             = 'InSearch',
-    version          = '0.1',
+    version          = '0.1.3',
     description      = 'Python-based inverted index structure library with full-text search support',
+    long_description = long_description,
+    long_description_content_type='text/markdown',
     author           = '2ternal, Fortune00, MoonDD99, shellboy',
-    author_email     = 'ddft97@gmail.com, sinyoungbok99@gmail.com, mjh991016@naver.com, qkrdmstlr3@naver.com',
+    author_email     = 'ddft97@gmail.com',
     url              = 'https://github.com/CNUCSE-Team2/InSearch',
     install_requires = [ 'eunjeon >= 0.4.0'],
     packages         = find_packages(exclude = []),
